cc22ceecfd8f84da598834a573c275978dcf46d4
Finderchangchang <1031066280@qq.com>
2016-12-11 23:37:54 +0800

    Initial commit

---
+++ app/src/main/java/yy/pwdmanager/ui/MainActivity.java
@@ -0,0 +1,92 @@
+package yy.pwdmanager.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import yy.pwdmanager.BaseActivity;
+import yy.pwdmanager.R;
+
+public class MainActivity extends BaseActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @Override
+    public void initViews() {
+        setContentView(R.layout.ac_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        fab.setOnClickListener(view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show());
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void initEvents() {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/yy/pwdmanager/ui/SSLoginActivity.java
@@ -0,0 +1,69 @@
+package yy.pwdmanager.ui;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import net.tsz.afinal.view.MaterialLockView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import yy.pwdmanager.BaseActivity;
+import yy.pwdmanager.R;
+import yy.pwdmanager.util.Utils;
+
+/**
+ * 手势登录页面
+ * Created by Finder丶畅畅 on 2016/12/11 21:59
+ * QQ群481606175
+ */
+
+public class SSLoginActivity extends BaseActivity {
+    @Bind(R.id.pwd_mv)
+    MaterialLockView pwdMv;
+    @Bind(R.id.msg_tv)
+    TextView msgTv;
+    String ss_key;
+    String re_key;//确认密码
+
+    @Override
+    public void initViews() {
+        setContentView(R.layout.ac_ss_login);
+        ButterKnife.bind(this);
+        ss_key = Utils.getCache("ss_key");
+    }
+
+    @Override
+    public void initEvents() {
+        pwdMv.setOnPatternListener(new MaterialLockView.OnPatternListener() {
+            @Override
+            public void onPatternDetected(List<MaterialLockView.Cell> pattern, String SimplePattern) {
+                if (!("").equals(ss_key)) {//登录
+                    if (ss_key.equals(SimplePattern)) {
+                        Utils.IntentPost(MainActivity.class);
+                        finish();
+                    } else {
+                        ToastShort("手势错误");
+                        pwdMv.clearPattern();
+                    }
+                } else {//注册
+                    if (re_key != null) {
+                        if (re_key.equals(SimplePattern)) {
+                            Utils.IntentPost(MainActivity.class);
+                            Utils.putCache("ss_key", re_key);
+                            finish();
+                        } else {
+                            ToastShort("手势错误");
+                            pwdMv.clearPattern();
+                        }
+                    } else {
+                        re_key = SimplePattern;
+                        pwdMv.clearPattern();
+                    }
+                }
+                super.onPatternDetected(pattern, SimplePattern);
+            }
+        });
+    }
+}
